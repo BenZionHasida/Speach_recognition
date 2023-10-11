017a8af84186151103ef64c9141640578e049e1e
BenZionHasida <134550781+BenZionHasida@users.noreply.github.com>
2023-10-11 12:28:54 +0300

    Update vite.config.js

--- speachRecognition/vite.config.js
+++ speachRecognition/vite.config.js
@@ -4,4 +4,6 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+    base: 'https://github.com/BenZionHasida/Speach_recognition/'
+
 })
